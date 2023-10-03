16a2faada03d3f4ae88da489b51678a0c397a5b0
Chika Okafor <ochika6363@gmail.com>
2023-10-03 15:37:13 -0700

    done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,5 +4,5 @@ for i in range(ord('z'), ord('a') - 1, -1):
     if n % 2 == 0:
         print("{:c}".format(i), end='')
     else:
-        print("{:c}".format(i-32), end='') 
+        print("{:c}".format(i-32), end='')
     n += 1
