8964bd436e2fd78fc850995da1cad32248583bc6
Chika Okafor <ochika6363@gmail.com>
2023-10-03 15:00:01 -0700

    done

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1,100):
-        if i == 99:
-            print('Fizz', end=' ')
+    for i in range(1,101):
+        if i == 100:
+            print('Buzz')
         elif i % 3 == 0 and i % 5 == 0:
             print('FizzBuzz', end=' ')
         elif i % 3 == 0:
