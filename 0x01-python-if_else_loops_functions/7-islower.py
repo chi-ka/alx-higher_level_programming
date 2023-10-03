e5cc871c965c41d2a3c70f8034c2e8f8cc15dd9e
Chika Okafor <ochika6363@gmail.com>
2023-10-03 11:50:21 -0700

    done

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    if c in "abcdefghijklmnopqrstuvwxyz":
-        return True
-    elif c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        return False
+    return ord('a') <= ord(c) <= ord('z')
