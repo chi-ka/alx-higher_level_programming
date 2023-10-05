938f3c39a722ecd11a8b92680412d069c88def14
Chika Okafor <ochika6363@gmail.com>
2023-10-05 15:21:35 -0700

    done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
 a = 1
 b = 2
-from add_0 import add
-result = add(a,b)
+result = add(a, b)
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, result), end="\n")
