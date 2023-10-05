5fc5d40665f14a44a9596ce914da3ed59fb68b86
Chika Okafor <ochika6363@gmail.com>
2023-10-05 13:59:01 -0700

    done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ a = 1
 b = 2
 from add_0 import add
 result = add(a,b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, result), end="\n")
