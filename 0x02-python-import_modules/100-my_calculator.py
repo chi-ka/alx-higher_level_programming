604b6a459a199d6fee3031feac5e34f97829442c
Chika Okafor <ochika6363@gmail.com>
2023-10-05 16:08:38 -0700

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
     if num_arguments != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = sys.argv[1]
-    b = sys.argv[3]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if (sys.argv[2] == '+'):
-        print("{0} + {1} = {2}".format(a, b, add(int(a, b))))
+        print("{0} + {1} = {2}".format(a, b, add(a, b)))
     elif (sys.argv[2] == '-'):
-        print("{0} - {1} = {2}".format(a, b, add(int(a, b))))
+        print("{0} - {1} = {2}".format(a, b, sub(a, b)))
     elif(sys.argv[2] == '*'):
-        print("{0} * {1} = {2}".format(a, b, add(int(a, b))))
+        print("{0} * {1} = {2}".format(a, b, mul(a, b)))
     elif (sys.argv[2] == '/'):
-        print("{0} / {1} = {2}".format(a, b, add(int(a, b))))
+        print("{0} / {1} = {2}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
