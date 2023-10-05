7661721c6c82aace9ae144bf28600a25bfbda087
Chika Okafor <ochika6363@gmail.com>
2023-10-05 14:26:52 -0700

    done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
-i = 0
-num_arguments = len(sys.argv) - 1
-if num_arguments == 0:
-    print("0")
-else:
-    for arg in sys.argv[1:]:
-        i += int(arg)
-    print("{0}".format(i))
+if __name__ == "__main__":
+    i = 0
+    num_arguments = len(sys.argv) - 1
+    if num_arguments == 0:
+        print("0")
+    else:
+        for arg in sys.argv[1:]:
+            i += int(arg)
+        print("{0}".format(i))
