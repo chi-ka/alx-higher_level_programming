13623322098f441875c912e7bf8824a5b685e69c
Chika Okafor <ochika6363@gmail.com>
2023-10-11 20:06:11 -0700

    done

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    summ = []
-    for key in a_dictionary.keys():
-        summ.append(key)
-        sorted_key = sorted(summ)
-    for key in sorted_key:
-        print(f"{key} : {a_dictionary[key]}")
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
