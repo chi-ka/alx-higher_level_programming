8f97541729964236f0e348df262c344850535b47
Chika Okafor <ochika6363@gmail.com>
2023-10-23 14:42:08 -0700

    done

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except(ValueError, NameError):
         error_message = "Exception: " + str(sys.exc_info()[1])
         sys.stderr.write(error_message + '\n')
         return False
