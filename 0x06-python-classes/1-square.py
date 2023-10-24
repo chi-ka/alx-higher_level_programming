feba7c19823d2c0fbf414de252a4579975b1b5ce
Chika Okafor <ochika6363@gmail.com>
2023-10-24 06:59:19 -0700

    done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 class Square:
+    """
+    This is a simple class that defines a square.
+
+    The Square class is used to represent a square shape with a single attribute,
+    '__size', which stores the size of all sides of the square. The '__size' attribute
+    is encapsulated using double underscores to make it a private attribute.
+    """
     def __init__(self, size):
         self.__size = size
