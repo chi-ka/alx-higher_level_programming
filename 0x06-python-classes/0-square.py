5802ab5abebaeb9205262aa3a2c3dcfd07ae9844
Chika Okafor <ochika6363@gmail.com>
2023-10-24 06:44:27 -0700

    done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """ This is a simple class that defines a square. """
+    """
+    This is a simple class that defines a square.
+
+    The Square class is used to represent a square shape with a single attribute,
+    'side_length', which stores the length of all sides of the square. This class
+    does not contain any methods or functions.
+    """
     pass
