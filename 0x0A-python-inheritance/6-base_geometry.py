99b34811ad696d1c20cb045ec6027cdd4db647bd
Chika Okafor <ochika6363@gmail.com>
2023-11-07 02:37:55 -0800

    done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Create an empty class BaseGeometry"""
+
+
+class BaseGeometry():
+    """
+    An empty class BaseGeometry.
+
+    """
+    def area(self):
+        """Empty Function area"""
+        raise Exception("area() is not implemented")
