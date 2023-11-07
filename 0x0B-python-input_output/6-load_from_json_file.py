ecd70770a068c745663bb3a793cd0cce243d812d
Chika Okafor <ochika6363@gmail.com>
2023-11-07 14:57:07 -0800

    done

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,19 +1,5 @@
 import json
 
-def save_to_json_file(my_obj, filename):
-    """
-    Serialize the given object to a JSON representation and write it to the specified file.
-
-    Args:
-        my_obj (object): The object to be serialized to JSON.
-        filename (str): The name of the file to save the JSON representation.
-
-    Returns:
-        None
-    """
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
-
 def load_from_json_file(filename):
     """
     Load an object from a JSON file and return it.
