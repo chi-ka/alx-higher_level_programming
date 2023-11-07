f72f4cab9166a2b0b810116162c4a2acc5dc9a61
Chika Okafor <ochika6363@gmail.com>
2023-11-07 06:30:30 -0800

    done

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Create an empty class BaseGeometry"""
+
+
+class BaseGeometry():
+    """
+    An empty class BaseGeometry.
+
+    """
+
+    def area(self):
+        """Empty Function area"""
+        
+
+    def integer_validator(self, name, value):
+        """Validate an integer"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    An class inherited from BaseGeometry.
+
+    """
+    def __init__(self, width, height):
+        """Initialiizng Class"""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ Get's the area of a Rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the sring"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
