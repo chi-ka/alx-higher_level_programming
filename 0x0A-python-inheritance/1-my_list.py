1431bc28e4eb1ade13bde9ba40a693dcaec244ac
Chika Okafor <ochika6363@gmail.com>
2023-11-07 01:04:12 -0800

    done

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ class MyList(list):
     """
     A class MyList that inherits from list
     """
-    def __init__(self):
-        """ Init main function"""
-        list.__init__(self)
-
     def print_sorted(self):
         """ Prints sorted list"""
         print(sorted(self))
