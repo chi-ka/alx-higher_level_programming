6a5edb09ca125e5a7f0b9a1a638f998d450141c7
Chika Okafor <ochika6363@gmail.com>
2023-11-07 12:18:03 -0800

    done

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ A function that adds a new attribute to an object"""
 
+
 def add_attribute(obj, attr, value):
     """adds a new attribute"""
     if hasattr(obj, '__dict__'):
