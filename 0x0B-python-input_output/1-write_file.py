fafe63ef95fee77168d080be4187c6208e33d035
Chika Okafor <ochika6363@gmail.com>
2023-11-08 14:33:29 -0800

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,10 +12,6 @@ def write_file(filename="", text=""):
     int: The number of characters written to the file.
 
     """
-    try:
-        with open(filename, 'w', encoding='utf-8') as file:
-            num_chars_written = file.write(text)
+    with open(filename, 'w', encoding='utf-8') as file:
+        num_chars_written = file.write(text)
         return num_chars_written
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        return 0
