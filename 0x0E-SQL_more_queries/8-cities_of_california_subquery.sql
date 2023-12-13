9a98826d60ea6be55ce82884b0df473b5c2a3241
Chika Okafor <ochika6363@gmail.com>
2023-12-13 13:13:59 -0800

    done

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- List all the cities of California without using JOIN
+USE hbtn_0d_usa;
+
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
