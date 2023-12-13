4105f8948dc2d1e2be0168be1e2dfb7c4102053a
Chika Okafor <ochika6363@gmail.com>
2023-12-13 12:11:05 -0800

    done

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create the user if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
