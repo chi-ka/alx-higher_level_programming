24c4b7cd2cd172b7e7b711b4dcf1bf8a9a5d189f
Chika Okafor <ochika6363@gmail.com>
2023-12-12 16:26:23 -0800

    done

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,2 @@
---Avv temp ordered by temp
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+-- Avv temp ordered by temp
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
