a042444c012f3bed1f96f72fa1f55e3d37170d77
Chika Okafor <ochika6363@gmail.com>
2023-12-13 16:57:31 -0800

    done

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,11 @@
--- List all shows by their ratings
-SELECT tv_shows.title, SUM(tvshows_rate.rating) AS rating_sum
+-- List all shows with their genres and ratings
+SELECT
+    tv_shows.title,
+    GROUP_CONCAT(DISTINCT tv_genres.name ORDER BY tv_genres.name ASC) AS genres,
+    IFNULL(SUM(tv_show_ratings.rate), 0) AS rating_sum
 FROM tv_shows
-JOIN tvshows_rate ON tv_shows.id = tvshows_rate.show_id
-GROUP BY tv_shows.id, tv_shows.title
-ORDER BY rating_sum DESC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating_sum DESC, tv_shows.title ASC;
