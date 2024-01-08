4f2d38bcbcea669c8098771a563523777036ff4d
Chika Okafor <ochika6363@gmail.com>
2024-01-08 05:14:08 -0800

    done

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const FirstLine = "C is fun" 
+const SecondLine = "Python is fun" 
+const ThirdLine = "Javascript is amazing"
+console.log( FirstLine )
+console.log(SecondLine)
+console.log(ThirdLine)
