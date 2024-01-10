bc746579e57c505add7af4047e7c14b3d8b5253e
Chika Okafor <ochika6363@gmail.com>
2024-01-10 15:00:37 -0800

    done

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Function to reverse a list
+exports.esrever = function (list) {
+    let reversedList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+        reversedList.push(list[i]);
+    }
+    return reversedList;
+};
+
+// Test the function
+const esrever = require('./8-esrever').esrever;
+
+console.log(esrever([1, 2, 3, 4, 5]));
+console.log(esrever(["School", 89, { id: 12 }, "String"]));
+ 
