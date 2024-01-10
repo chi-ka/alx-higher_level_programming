10e9283bbaf9924aaa8d2fd42dd4c5e9ac3cea8b
Chika Okafor <ochika6363@gmail.com>
2024-01-10 11:54:27 -0800

    done

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // This class is intentionally left empty
+}
+
+const r1 = new Rectangle();
+console.log(r1); // Expected output: Rectangle {}
+console.log(r1.constructor); // Expected output: [Function: Rectangle]
+
+
