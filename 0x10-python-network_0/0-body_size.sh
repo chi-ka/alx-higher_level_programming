b002a5192d683a6ddd60596a83600db2270e8d09
chika okafor <ochika6363@gmail.com>
2024-01-29 07:21:00 -0800

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-URL=$1
-BODY_SIZE=$(curl -s -o /dev/null --write-out '%{size_download}' "$URL")
-echo $BODY_SIZE
+# This script takes a URL as input and outputs the size of the response body in bytes
+URL=$1; BODY_SIZE=$(curl -s -o /dev/null --write-out '%{size_download}' "$URL"); echo $BODY_SIZE
