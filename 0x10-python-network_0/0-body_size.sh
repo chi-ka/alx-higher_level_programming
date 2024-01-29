e25dacf1227a0db3e34c0fec15ba696170c4a6da
chika okafor <ochika6363@gmail.com>
2024-01-29 07:10:26 -0800

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Assign the first argument (URL) to a variable
 URL=$1
-
-# Use curl to get the body size and assign it to a variable
 BODY_SIZE=$(curl -s -o /dev/null --write-out '%{size_download}' "$URL")
-
-# Display the body size
 echo $BODY_SIZE
