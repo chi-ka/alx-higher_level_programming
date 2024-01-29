fec2526a9d6d431ba63a7f095d735a1d2c0f40f3
chika okafor <ochika6363@gmail.com>
2024-01-29 14:12:40 -0800

    done

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST request to a given URL with the letter
+as a parameter. Displays certain information based on the JSON response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    response = requests.post(url, data={'q': letter})
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
