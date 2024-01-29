a7332fcce917eb7529e1b1dc912b25afae59553b
chika okafor <ochika6363@gmail.com>
2024-01-29 14:19:27 -0800

    done

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that takes GitHub credentials (username and personal access token) and uses
+the GitHub API to display the user's ID.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, token))
+
+    if response.status_code == 200:
+        print(response.json().get('id'))
+    else:
+        print("None")
