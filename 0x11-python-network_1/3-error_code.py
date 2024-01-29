66e35cedb7d53d7cdfb580b1d857bf3de84cd17f
chika okafor <ochika6363@gmail.com>
2024-01-29 13:59:15 -0800

    done

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends a request to the URL, and displays the body
+of the response. Manages urllib.error.HTTPError exceptions to print the HTTP
+status code.
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            print(content.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
