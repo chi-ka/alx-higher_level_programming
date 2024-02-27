fbaf8568769ca54198c9545aeefc827d137bf5ce
Chika OKafor <ochika6363@gmail.com>
2024-02-27 10:18:40 -0800

    done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -35,7 +35,7 @@ def list_states(username, password, database):
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
 
     states = cursor.fetchall()
 
