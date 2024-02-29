9a03bc7f35f50cc1416a0e2048dd2389c0eec224
Chika Okafor <ochika6363@gmail.com>
2024-02-29 06:23:20 -0800

    done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
-const arg = process.argv[2];
-if (!arg){
-  console.log('No argument ')
-}else{
-  console.log(arg);
+if (process.argv[2] === undefined) {
+    console.log("No argument");
+} else {
+    console.log(process.argv[2]);
 }
-
