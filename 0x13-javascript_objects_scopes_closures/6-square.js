6edb53a26627d03cdcc32ae7943387f807fb2906
Chika OKafor <ochika6363@gmail.com>
2024-02-29 04:22:01 -0800

    done

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,56 +1,15 @@
 #!/usr/bin/node
+const BaseSquare = require('./5-square');
 
-// Rectangle class
-class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            return; 
-        }
-        this.width = w;
-        this.height = h;
-    }
-
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-        }
-    }
-
-    rotate() {
-        [this.width, this.height] = [this.height, this.width];
-    }
-
-    double() {
-        this.width *= 2;
-        this.height *= 2;
-    }
-}
-
-// Square class from 5-square.js
-class Square5 extends Rectangle {
-    constructor(size) {
-        super(size, size);
-    }
-}
-
-// New Square class that extends the Square5 class
-class Square extends Square5 {
-    constructor(size) {
-        super(size);
-    }
-
+class Square extends BaseSquare {
     charPrint(c) {
-        if (c === undefined) {
+        if (c === undefined)
             c = 'X';
-        }
-        for (let i = 0; i < this.height; i++) {
+
+        for (let i = 0; i < this.width; i++) {
             console.log(c.repeat(this.width));
         }
     }
 }
 
-// Main script
-const s1 = new Square(4);
-s1.charPrint(); // Using default 'X'
-s1.charPrint('C'); // Using 'C'
-
+module.exports = Square;
