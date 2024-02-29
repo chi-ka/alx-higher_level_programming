749d21754c3f61b598655b924c0ea505b5c7bd39
Chika Okafor <ochika6363@gmail.com>
2024-02-29 06:27:16 -0800

    done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
+const number = parseInt(process.argv[2]);
 
-const arg = process.argv[2]
-const number = parseInt(arg)
-
-if(!isNaN(number))
-{console.log(`My number: ${Number}`)
-}else{
-  console.log('Not a number')
+if (!isNaN(number)) {
+    console.log(`My number: ${number}`);
+} else {
+    console.log("Not a number");
 }
-
