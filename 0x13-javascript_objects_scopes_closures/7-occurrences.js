d80549a023f5961e948043fccb55b71bab55677b
Chika OKafor <ochika6363@gmail.com>
2024-02-29 04:29:27 -0800

    done

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
+exports.nbOccurences = function(list, searchElement) {
+    let count = 0;
 
-// 7-occurrences.js
-exports.nbOccurences = function (list, searchElement) {
-    return list.reduce((count, current) => (current === searchElement ? count + 1 : count), 0);
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === searchElement)
+        {
+            count++;
+        }
+    }
+
+    return count;
 };
 
-// 7-main.js
-const nbOccurences = require('./7-occurrences').nbOccurences;
-
-console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
-console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
-
+module.exports = Rectangle;
