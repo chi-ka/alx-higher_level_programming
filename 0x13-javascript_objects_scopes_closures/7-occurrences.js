c862cfd97bb1fe771b0d67085af9288a31001d63
Chika Okafor <ochika6363@gmail.com>
2024-02-29 04:43:59 -0800

    done

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
 exports.nbOccurences = function(list, searchElement) {
+    // Initialize a counter for occurrences
     let count = 0;
 
+    // Iterate over the list
     for (let i = 0; i < list.length; i++) {
-        if (list[i] === searchElement)
-        {
+        // Check if the current element is equal to the search element
+        if (list[i] === searchElement) {
+            // If equal, increment the counter
             count++;
         }
     }
 
+    // Return the count of occurrences
     return count;
 };
-
-module.exports = Rectangle;
