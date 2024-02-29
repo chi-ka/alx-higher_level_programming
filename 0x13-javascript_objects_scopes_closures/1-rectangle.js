411894fe2469ad17d55aaeb770613da9c396c542
Chika OKafor <ochika6363@gmail.com>
2024-02-29 03:39:13 -0800

    done

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,26 +1,8 @@
 #!/usr/bin/node
-
-// Define the Rectangle class
 class Rectangle {
-  constructor(w, h) {
-    this.width = w;
-    this.height = h;
-  }
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    }
 }
-
-// Usage of the Rectangle class
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log('Width:', r1.width);
-console.log('Height:', r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log('Width:', r2.width);
-console.log('Height:', r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log('Width:', r3.width);
-console.log('Height:', r3.height);
-
+module.exports = Rectangle;
