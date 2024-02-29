0f139e6a242d4c65f16240ed781139865e0f099e
Chika Okafor <ochika6363@gmail.com>
2024-02-29 05:30:41 -0800

    done

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,24 +1,10 @@
 #!/usr/bin/node
-
-// Function to create a converter for a specific base
-exports.converter = function (base) {
-  return function (number) {
-    return number.toString(base);
-  };
+exports.converter = function(base) {
+    return function convert(number) {
+        if (number < base) {
+            return number.toString(base);
+        } else {
+            return convert(parseInt(number / base)) + (number % base).toString(base);
+        }
+    };
 };
-
-// Test the function
-const converter = require('./10-converter').converter;
-
-let myConverter = converter(10);
-
-console.log(myConverter(2));  // 2 in base 10
-console.log(myConverter(12)); // 12 in base 10
-console.log(myConverter(89)); // 89 in base 10
-
-myConverter = converter(16);
-
-console.log(myConverter(2));  // 2 in base 16 (hexadecimal) is "2"
-console.log(myConverter(12)); // 12 in base 16 is "c"
-console.log(myConverter(89)); // 89 in base 16 is "59"
-
