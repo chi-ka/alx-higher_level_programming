d850d79ccc4e051b21eb32ee1f0394d8d5c8212f
Chika OKafor <ochika6363@gmail.com>
2024-05-29 00:39:43 -0700

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file
 const fs = require('fs');
-const path = require('path');
 
 // Check if a file path is provided as an argument
 if (process.argv.length < 3) {
-    console.log("Usage: node 0-readme.js <file path>");
-    process.exit(1);
+  console.log('Usage: node 0-readme.js <file path>');
+  process.exit(1);
 }
 
 const filePath = process.argv[2];
 
 // Read the file using utf-8 encoding
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
